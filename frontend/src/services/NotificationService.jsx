72491fa21aa8a14a6320a1c911268f2a913add00
Sujal Raj <sujalraj2064@gmail.com>
2025-03-15 14:47:56 +0530

    Cleared notice form submission and made admin id, pass in env

--- frontend/src/services/NotificationService.jsx
+++ frontend/src/services/NotificationService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api"; // Adjust according to your backend
+const API_BASE_URL = "http://localhost:3000/api";
 
 // Fetch events
 export const getEventByDate = async (date) => {
@@ -15,15 +15,23 @@ export const getEventByDate = async (date) => {
 
 // Fetch notices
 export const getNotices = async () => {
-  const response = await axios.get(`${API_BASE_URL}/notices`);
-  return response.data;
+  try{
+    const response = await axios.get(`${API_BASE_URL}/notices`);
+    return response.data;
+  }catch(error){
+    return error.response.data;
+  }
 };
 
 // Add notice (with file upload)
 export const addNotice = async (formData) => {
   console.log(formData);
-  const response = await axios.post(`${API_BASE_URL}/notices/add`, formData, {
-    headers: { "Content-Type": "multipart/form-data" },
-  });
-  return response.data;
+  try{
+    const response = await axios.post(`${API_BASE_URL}/notices/add`, formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    return response.data;
+  }catch(error){
+    return error.response.data;
+  }
 };
