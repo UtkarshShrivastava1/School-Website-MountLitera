2b5f2017b4884da68b26597115ecc4e1c4f04d0f
DEVENDRA DILLIWAR <devendradilliwar30@gmail.com>
2025-03-21 17:39:03 +0530

    merge

--- frontend/src/pages/contact/Careers.jsx
+++ frontend/src/pages/contact/Careers.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import Header from '../../components/Home/Navbar'
-import Footer from '../../components/Home/Footer'
 import CareersPage from '../../components/contact/Steps'
 
 const Careers = () => {
   return (
     <>
-        <Header />
+        
         <CareersPage />
-        <Footer />
+       
     </>
   )
 }
